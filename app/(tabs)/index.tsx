4181c7bd087ee596b7d999e94c3003efb4477a89
Y-ash-Thakur <yasht0509@gmail.com>
2025-07-20 18:21:27 +0530

    Publishing and implementing SafeAreaView

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,17 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, Text} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
-  const hour = 24
-  // new Date().getHours();
-  let greeting;
-
-  if (hour < 12) {
-    greeting = 'Good morning';
-  } else if (hour < 18) {
-    greeting = 'Good afternoon';
-  } else if (hour < 24) {
-    greeting = 'Good evening';
-  } else {
-    greeting = "Hello"
-  }
-
-  console.log('Hour is:', hour);
-  console.log('Greeting is:', greeting);
-
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{greeting}, Yash 👋</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello Yash 😀</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: 'dodgerblue',
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
