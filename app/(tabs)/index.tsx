a2f318d2b4d068b819b1b2c12cfa0286b8bf565b
Y-ash-Thakur <yasht0509@gmail.com>
2025-07-20 19:41:08 +0530

    View, Text, Images, Touchables implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import { StyleSheet, Text} from 'react-native';
+import { Image, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello Yash 😀</Text>
+      <Text >Hello Yash 😀</Text>
+      <View style={{width: 200, height: 70, backgroundColor: 'dodgerblue'}}></View>
+      <TouchableHighlight onPress={() => {console.log("Image Tapped")}}>
+      <Image
+      source={{
+                        width: 200,
+                        height: 300,
+                        uri: "https://picsum.photos/200/300"}} />
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -12,6 +21,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
       flex: 1,
-      backgroundColor: 'dodgerblue',
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center'
   },
 });
